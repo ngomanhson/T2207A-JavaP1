e40e60227a368baa9b9b08bf19de2b1e494b6b1b
NgoManhSon <ngomanhson2004txpt@gmail.com>
2023-02-01 16:24:09 +0700

    Nop bai tap 1/2/23

---
+++ src/Session7/INews.java
@@ -0,0 +1,5 @@
+package Session7;
+
+public interface INews {
+    void Display();
+}
---
+++ src/Session7/News.java
@@ -0,0 +1,85 @@
+package Session7;
+
+public class News implements INews {
+    private int ID;
+    private String Title;
+    private String PublishDate;
+    private String Author;
+    private String Content;
+    private float AverageRate;
+
+    private int RateList[] = new int[3];
+    public News(int ID, String title, String publishDate, String author, String content, float averageRate) {
+        this.ID = ID;
+        Title = title;
+        PublishDate = publishDate;
+        Author = author;
+        Content = content;
+        AverageRate = averageRate;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getPublishDate() {
+        return PublishDate;
+    }
+
+    public void setPublishDate(String publishDate) {
+        PublishDate = publishDate;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public float getAverageRate() {
+        return AverageRate;
+    }
+
+    public void setAverageRate(float averageRate) {
+        AverageRate = averageRate;
+    }
+
+    @Override
+    public void Display() {
+        System.out.println("Title: " + Title);
+        System.out.println("Publish Date: " + PublishDate);
+        System.out.println("Author: " + Author);
+        System.out.println("Content: " + Content);
+        System.out.println("Average Rate: " + AverageRate);
+    }
+
+    public void Calculate(){
+        int s = 0;
+        for (int i : RateList){
+            s += i;
+        }
+        AverageRate = (float) s / RateList.length;
+    }
+}
